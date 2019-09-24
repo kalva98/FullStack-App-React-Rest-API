bd0456879df9382a0fc086a01c808df132d554ad
Karen Alvarado <karen.alvarado@one.verizon.com>
2019-09-24 17:24:18 -0400

    5:24pm

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,15 @@ import Header from './components/Header';
 import CourseDetails from './components/CourseDetails';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
+import UserSignIn from './components/UserSignIn';
+import withContext from './components/Context';
+
+const CoursesWithContext = withContext(Courses);
+const CourseDetailsWithContext = withContext(CourseDetails);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
+const UserSignInWithContext = withContext(UserSignIn);
+const HeaderWithContext = withContext(Header);
 
 export default class App extends Component{
 
@@ -14,13 +23,15 @@ export default class App extends Component{
     return (
 
       <div className="container">
-        <Header/>
+        
         <BrowserRouter>
+        <HeaderWithContext/>
           <Switch>
-            <Route key={0} exact path="/" component={Courses} />
-            <Route path="/courses/:id" component={CourseDetails}/>
-            <Route path= "/course/create" component={CreateCourse}/>
-            <Route path= {`/course/:id/update`} component={UpdateCourse}/>
+            <Route exact path="/" component={CoursesWithContext} />
+            <Route path="/courses/:id" component={CourseDetailsWithContext}/>
+            <Route path= "/course/create" component={CreateCourseWithContext}/>
+            <Route path= {`/course/:id/update`} component={UpdateCourseWithContext}/>
+            <Route path= "/signin" component={UserSignInWithContext}/>
           </Switch>
         </BrowserRouter>
       </div>
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -42,12 +42,11 @@ class Courses extends Component {
                             </Link>
                         </div>)}
                     <div className="grid-33" >
-                        <Link className="course--module course--link" to="/course/create">
-                            <h3 className="course--add--title" ><svg version="1.1"
-                                xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+                        <Link className="course--module course--add--module" to="/courses/create">
+                            <h3 className="course--add--title" ><svg version="1.1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
                                 viewBox="0 0 13 13" className="add" >
                                 <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " ></polygon>
-                            </svg>New Course</h3 >
+                            </svg>New Course</h3>
                         </Link></div>
                 </div>             
             )
@@ -58,5 +57,5 @@ class Courses extends Component {
         }
                
     }
-        }     
+}     
 export default Courses
